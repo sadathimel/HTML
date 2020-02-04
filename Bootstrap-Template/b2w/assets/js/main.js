36cf23a54c255f9e995ec7fc7a589c4aa95c1012
Sadat Hossen Himel <40139304+sadathimel@users.noreply.github.com>
2020-02-04 13:15:25 +0600

    Add files via upload

---
+++ Bootstrap-Template/b2w/assets/js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+    // cache the window object
+    var $window = $(window);
+
+    // parallax background effect
+    $('section[data-type="background"]').each(function() {
+
+        var $bgobj = $(this); //assigning the object
+
+        $(window).scroll(function(){
+
+            // scroll the background at var speed
+            // the yPos is a negative value because we're sctolling it up!
+
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+            // put together our final background position
+            var coords = '50% '+yPos + 'px';
+
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+
+        }); //end window scroll
+    });
+});
